061c30b1e1c3bb2eb419229bf06dcbbeb3e17349
Peter Dinis <peterdinis0@pobox.sk>
2021-01-14 16:18:57 +0100

    Edit product ui

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -3,11 +3,30 @@ import styles from '../styles/CheckoutForm.module.css';
 
 export default function CheckoutForm() {
     const stripe = useStripe();
+    const elements = useElements();
+
+    const handlePaySubmit = async (e) => {
+        e.preventDefault();
+
+        const [error, paymentMethod] = stripe.createPaymentMethod({
+            type: 'card',
+            card: elements.getElement(CardElement)
+        });
+
+        if(!error) {
+            console.log(paymentMethod)
+        }
+    }
+
+    // https://dashboard.stripe.com/test/dashboard
+
     return (
         <div className={styles.paymentForm}>
-            <form>
+            <form onSubmit={handlePaySubmit}>
+                <h2 className={styles.header}>Price: 6€</h2>
+                <img className={styles.tshirt} src='https://mockup-api.teespring.com/v3/image/N_lzOWHSYHVOeVAgT0B31wcGvEU/480/560.jpg' alt='Tshirt-Image' />
                 <CardElement />
-                    <button disabled={stripe} className={styles.payment}>
+                    <button type='submit' disabled={stripe} className={styles.payment}>
                             Pay
                     </button>
              </form>
